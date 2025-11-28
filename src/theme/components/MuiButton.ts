0cac4bf9edef70ba36a313cd755d68ce484688f1
Ankit-cms <SoloLeveling-git>
2025-11-28 23:10:54 +0530

    Changed header with button style and added more color with the sections background on home page

--- src/theme/components/MuiButton.ts
+++ src/theme/components/MuiButton.ts
@@ -10,9 +10,14 @@ const MuiButton: Components['MuiButton'] = {
     contained: {
       boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
       backgroundColor: palette.common.black,
+      border: '1px solid transparent',
+      transition: 'all 0.3s ease',
+      color: palette.common.white,
       '&:hover': {
         boxShadow: '0 4px 8px rgba(0,0,0,0.15)',
         backgroundColor: palette.common.white,
+        border: '1px solid ' + palette.common.black,
+        color: palette.common.black,
       },
       '&:active': {
         boxShadow: '0 2px 4px rgba(0,0,0,0.15)',
